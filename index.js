9fc8571a40118489d1094a2fa039c74cf65f51f7
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-01-13 19:59:34 -0600

    Added a function called generateReadme that will prompt users with questions, user answers will be stored so they can be used in the generation of the readme file

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
+
+
+let generateReadme = () => inquirer.prompt([
+
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+
+    {
+        type: '',
+        message: '',
+        name: ''
+    },
+
+    {
+        type: '',
+        message: '',
+        name: ''
+    },
+
+    {
+        type: '',
+        message: '',
+        name: ''
+    },
+
+    {
+        type: '',
+        message: '',
+        name: ''
+    },
+
+])
